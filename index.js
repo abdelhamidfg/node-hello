4a8890c626a72936ebec5b8bfb00c8a778e4f606
abdelhamidfg <abdelhamidfg@gmail.com>
2021-06-26 23:21:26 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = `Hello Node! ${port} ${process.env.MSG}\n`
+  const msg = `Hello Node! ${port} ${process.env.MSG} ${process.env.dbpassword}\n`
   res.end(msg);
 });
 
